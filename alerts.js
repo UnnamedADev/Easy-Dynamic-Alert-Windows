d287f74660354a867b8c411434a17fad19defc00
Unnamed Developer <unnamed.a.dev@gmail.com>
2018-01-13 19:10:28 +0100

    v0.0.3
    HTML
    + Added special attributes for alerts handling
    CSS
    + Removed temporary alerts holder background color
    JAVASCRIPT
    + Added arguments handling for function that builds alerts
    + Fixed structure's position of alert paragraphs
    + Added custom alert for error in alert type handling

--- alerts.js
+++ alerts.js
@@ -19,23 +19,40 @@ $(document).ready(function() {
         
         for(var i=0;i<allReactors.length;i++){
             allReactors[i].addEventListener("click", function(){
-                buildAlert();
+                var type = this.getAttribute("alertType");
+                var content = this.getAttribute("alertContent");
+                buildAlert(type, content);
             });
         }
     }
     
-    function buildAlert(){
-        
-        console.log("xd");
+    function buildAlert(type,content){
+
         var myHolder = document.getElementById("alertHolder");
        
         var newAlert = document.createElement("div");
         newAlert.classList.add("alert");
         myHolder.appendChild(newAlert);
-        newAlert.style.backgroundColor = "blue";
+        var alertColor;
+        switch(type){
+            case "neutral":
+                alertColor="yellow";
+                break;
+            case "negative":
+                alertColor = "red";
+                break;
+            case "positive":
+                alertColor = "green";
+                break;
+            default:
+                window.alert("#ERROR: \n-Invalid alert type.\n -Check in HTML code element that provides this event for any errors in name of 'alertType' attribute. This attribute can be only: neutral / positive / negative.")
+                break;
+        }
+
+        newAlert.style.backgroundColor = alertColor;
         
         var newParagraph = document.createElement("p");
-        myHolder.appendChild(newParagraph);
-        newParagraph.innerHTML = "hehe";
+        newAlert.appendChild(newParagraph);
+        newParagraph.innerHTML = content;
     }
 });
